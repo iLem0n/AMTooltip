cadb0350a8836ec6ea559d7db1a90379b5ec0302
iLem0n <peter.c.glade@googlemail.com>
2018-10-23 20:50:06 +0200

    Update AMTooltipView.swift

--- AMTooltip/Classes/AMTooltipView.swift
+++ AMTooltip/Classes/AMTooltipView.swift
@@ -417,8 +417,8 @@ open class AMTooltipView: UIView {
         
         
         //check popup in window frame
-       
-            let spaceFromLeftSide = cutOutViewX.constant + cutOutViewWidth.constant/2 - (options.textWidth + padding*2)/2
+            let padding = (options.textWidth + padding*2)/2
+            let spaceFromLeftSide = cutOutViewX.constant + cutOutViewWidth.constant/2 - padding
             
             if spaceFromLeftSide < 0{
                 
@@ -430,7 +430,7 @@ open class AMTooltipView: UIView {
                 }
             }
             
-            let spaceFromRightSide = cutOutViewX.constant + cutOutViewWidth.constant/2 + (options.textWidth + padding*2)/2
+            let spaceFromRightSide = cutOutViewX.constant + cutOutViewWidth.constant/2 + padding
             
             if spaceFromRightSide > targetView.frame.size.width{
                 
